0d09cdeb075a251eb344fdec2786aaacf7a6cf13
JSGong5 <jsgong94@gmail.com>
2023-04-24 15:23:32 +0900

    sql init 기능 추가

--- 프로그래머스/main.py
+++ 프로그래머스/main.py
@@ -1,6 +1,12 @@
 import argparse
 import re
+import inspect
+import os
 
+from typing import Union, Dict, Tuple
+
+def get_stack() -> str:
+    return inspect.stack()[2].function
 class CustomParser:
     parser = argparse.ArgumentParser()
 
@@ -22,6 +28,12 @@ class CustomParser:
         add optional argument
         '''
         self.opt.add_argument(*args, **kwargs, required=False)
+
+    def add_bool(self, *args, **kwargs):
+        '''
+        add boolean argument (on/off)
+        '''
+        self.opt.add_argument(*args, **kwargs, required=False, action="store_true")
     
     @classmethod
     def parse_args(cls):
@@ -36,31 +48,68 @@ class CustomParser:
         )
 
 class Initializer:
+    PATH:Dict[str, str] = {
+        "python": "./Python",
+        "sql": "./MySQL"
+    }
+    EXTENSION:Dict[str, str] = {
+        "python": "py",
+        "sql": "sql"
+    }
+    
+    SAMPLE_FILE_NAME = "sample"
+    
+    PROGRAMMERS_URL = "https://school.programmers.co.kr/learn/courses/30/lessons"
 
     def __init__(self, lang:str, command:str, **kwargs):
         self.cmd_type = f"_{command}_{lang}"
         self.main(**kwargs)
     
+    @property
+    def path(self):
+        frm = get_stack().split("_")[-1]
+        return self.PATH.get(frm, None)
+
+    @property
+    def ext(self):
+        frm = get_stack().split("_")[-1]
+        return self.EXTENSION.get(frm, None)
+
     def main(self, **kwargs):
         getattr(self, self.cmd_type)(**kwargs)
 
     def _init_python(self, idx, name, **kwargs):
-        with open("./Python/sample.py", "r") as f:
+        param:Union[None, str] = kwargs['param']
+        force:bool = kwargs['force']
+        file_name = f"{self.path}/{idx}_{name}.{self.ext}"
+        
+        if not force and os.path.isfile(file_name):
+            raise FileExistsError
+
+        with open(f"{self.path}/{self.SAMPLE_FILE_NAME}.{self.ext}", "r") as f:
             lines = "".join(f.readlines())
 
-        lines = lines.replace("@url", f" https://school.programmers.co.kr/learn/courses/30/lessons/{idx}")
+        lines = lines.replace("@url", f" {self.PROGRAMMERS_URL}/{idx}")
 
-        if kwargs['param'] is not None:
-            params = kwargs['param'].replace(",", '": None,\n\t\t"')
-            
+        if param is not None:
+            params = param.replace(",", '": None,\n\t\t"')
             lines = lines.replace("#@param", f'"{params}": None')
 
-        with open(f"./Python/{idx}_{name}.py", "w") as f:
+        with open(file_name, "w") as f:
             f.write(lines)
         
 
-    def _init_sql(self):
-        pass
+    def _init_sql(self, idx, name, **kwargs):
+        force:bool = kwargs['force']
+        file_name = f"{self.path}/{idx}_{name}.{self.ext}"
+
+        lines = f"-- {self.PROGRAMMERS_URL}/{idx}\n\n"
+
+        if not force and os.path.isfile(file_name):
+            raise FileExistsError
+        
+        with open(file_name, "w") as f:
+            f.write(lines)
 
     @classmethod
     def initialize(cls, **kwargs):
@@ -72,6 +121,7 @@ if __name__ == "__main__":
     parser_init.add_req("-n", "--name", type=str, help=">> problem name")
     parser_init.add_req("-l", "--lang", type=str, help=">> problem language")
     parser_init.add_opt("-p", "--param", type=str, help=">> comma seperated parameter names")
+    parser_init.add_bool("-f", "--force", help=">> force to overwrite the existing file")
     
     args = dict(vars(CustomParser.parse_args()))
     i = Initializer.initialize(**args)
