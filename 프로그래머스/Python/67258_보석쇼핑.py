e7d397df5742c6d2fadadb3214660c3f4fa12e46
unknown <jsgong@ontacthealth.com>
2023-08-02 15:27:05 +0900

    카카오 코딩테스트 문제풀이 추가

--- 프로그래머스/Python/67258_보석쇼핑.py
+++ 프로그래머스/Python/67258_보석쇼핑.py
@@ -1,43 +1,47 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/67258
 
 def solution(gems):
-    #초기화
-    endpoint = len(gems)
-    contain_dict = {g:0 for g in gems}
-    remain_kind = len(contain_dict.keys())
-    
-    contain_dict[gems[0]] = 1
-    remain_kind -= 1
-    
-    i,j = 0,1
-    answer = [i+1, j]
-    length = float('inf')
-    
-    while i<j and j<=endpoint:
-        if remain_kind == 0 and j-i < length:
-            answer = [i+1,j]
-            length = j-i
-        if remain_kind > 0:
-            if j == endpoint:
-                break
-            j += 1
-            contain_dict[gems[j-1]] += 1
-            if contain_dict[gems[j-1]] == 1:
-                remain_kind -= 1
+    max_length = len(gems)
+    gem_count = {gem:0 for gem in gems}
+    remains = len(gem_count.keys())
+
+    min_length = len(gem_count.keys())-1
+    optimal = 2*len(gems)
+    left, right = 1, 1
+    answer = [1, 1]
+
+    gem_count[gems[0]] += 1
+    remains -= 1
+
+    while left<=right:
+        print(f"left: {left}, right: {right}, remains: {remains}, gem_count: {gem_count}, gems: {gems[left-1:right]}")
+        if remains == 0:
+            if right-left+1 < optimal:
+                optimal = right-left+1
+                answer = [left, right]
+            gem_count[gems[left-1]] -= 1
+            if not gem_count[gems[left-1]]:
+                remains += 1
+            left += 1
+            if optimal == min_length:
+                return answer
         else:
-            i += 1
-            contain_dict[gems[i-1]] -= 1
-            if contain_dict[gems[i-1]] == 0:
-                remain_kind += 1
+            if right+1 > max_length:
+                return answer
+            if gem_count[gems[right]] == 0:
+                remains -= 1
+            gem_count[gems[right]] += 1
+            right += 1
 
     return answer
-        
-        
-    
+            
+            
+                
+                
 
 if __name__ == "__main__":
     param = {
-        "gems": ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
+        "gems": 	["AA", "AB", "AC", "AA", "AC"]
     }
-    result = [3, 7]
+    result = [1, 3]
     print(solution(**param), result)
--- 프로그래머스/Python/81302_거리두기확인하기.py
+++ 프로그래머스/Python/81302_거리두기확인하기.py
@@ -1,37 +1,38 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/81302
 
 from itertools import combinations
+
+def distance(r1, c1, r2, c2):
+    man_d = abs(r1-r2)+abs(c1-c2)
+    diag = False
+    if man_d==2 and abs(r1-r2)==1:
+        diag = True
+    return man_d, diag
+        
+def check(places, r1, c1, r2, c2):
+    dist, diag = distance(r1, c1, r2, c2)
+    if dist > 2:
+        return True
+    if dist == 2:
+        if not diag and places[(r1+r2)//2][(c1+c2)//2]=="X":
+            return True
+        if diag and places[r1][c2]==places[r2][c1]=="X":
+            return True
+    return False
+
 def solution(places):
-    answers = []
-    for p in places:
-        answer = 1
-        p = [list(i) for i in p]
-        person = []
-        for r in range(5):
-            for c in range(5):
-                if p[r][c]=='P': person.append((r,c))
-        cps = list(combinations(person,2))
-        for cp in cps:
-            r1,c1,r2,c2 = cp[0][0],cp[0][1],cp[1][0],cp[1][1]
-            md = abs(r1-r2)+abs(c1-c2)
-            if md>2: continue
-            elif md==1: 
-                answer=0
-                break
-            elif (md==2) and (r1==r2 or c1==c2):
-                if p[(r1+r2)//2][(c1+c2)//2] != "X":
-                    answer=0
-                    break
-            else:
-                if not (p[r2][c1]==p[r1][c2]=='X'):
-                    answer=0
-                    break
-        answers.append(answer)
-    return answers
+    return [1 if all([check(place, *p1, *p2) for p1, p2 in combinations([(r,c) for r, row in enumerate(place) for c, col in enumerate(row) if col=="P"], 2)]) else 0 for place in places]
+    answer = []
+    for place in places:
+        participants = [(r,c) for r, row in enumerate(place) for c, col in enumerate(row) if col=="P"]
+        result = [check(place, *p1, *p2) for p1, p2 in combinations(participants, 2)]
+        answer.append(1 if all(result) else 0)
+    
+    return answer
 
 if __name__ == "__main__":
     param = {
         "places": [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
     }
-    result = [1,0,1,1,1]
+    result = [1, 0, 1, 1, 1]
     print(solution(**param), result)
