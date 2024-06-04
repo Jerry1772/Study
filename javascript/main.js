dfd6abca3a327f2f0b65dc572dfd13526c79221f
jsgong <jsgong@ontacthealth.com>
2024-06-04 19:41:54 +0900

    js 추가

--- javascript/main.js
+++ javascript/main.js
@@ -8,7 +8,7 @@ console.log(add(1,2))
 myArray = [1,2,3,4,5]
 for(let i=0; i<10; i++) {
     if (i<5) {
-        console.log(myArray[4-i])
+        console.log(`문자열 포맷팅: ${myArray[4-i]}`)
     } else {
         console.log("pass")
     }
