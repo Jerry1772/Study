b95f81fa1e5982ae2bbbfd395ece4459a665eab7
jsgong <jsgong@ontacthealth.com>
2024-06-04 19:40:10 +0900

    함수선언, 기초문법

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,21 @@
-let a = 1;
-print(a)
+function add(x, y) {
+    let temp = x+y;
+    return temp
+}
+
+console.log(add(1,2))
+
+myArray = [1,2,3,4,5]
+for(let i=0; i<10; i++) {
+    if (i<5) {
+        console.log(myArray[4-i])
+    } else {
+        console.log("pass")
+    }
+}
+
+let tempSum = 0;
+myArray.forEach(element => {
+    console.log(element);
+});
+    
